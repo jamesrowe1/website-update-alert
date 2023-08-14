35347a9ce5a860aba3abaaee3d858b1326612bab
James Rowe <jamesrowe@outlook.com>
2023-08-14 15:36:23 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
+// src/App.js
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+import axios from 'axios';
 import './App.css';
 
+const socket = io.connect('http://localhost:5000'); // Replace with your server URL
+
 function App() {
+  const [content, setContent] = useState('');
+
+  useEffect(() => {
+    // Get initial content
+    axios.get('http://localhost:5000')
+      .then(response => {
+        setContent(response.data);
+      })
+      .catch(error => {
+        console.error('Error getting initial content:', error.message);
+      });
+
+    // Listen for website updates
+    socket.on('websiteUpdate', updatedContent => {
+      setContent(updatedContent);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Website Update Alert</h1>
+        <div dangerouslySetInnerHTML={{ __html: content }} />
       </header>
     </div>
   );
